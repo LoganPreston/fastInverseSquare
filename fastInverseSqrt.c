a183652d25578e97190e63037663b4608c6376f6
Logan P <70850609+LoganPreston@users.noreply.github.com>
2021-01-03 09:52:18 -0600

    adding fast inverse sqrt file straight up.
    may need to swap to header file to compare apples to apples.

---
+++ fastInverseSqrt.c
@@ -0,0 +1,14 @@
+float Q_rsqrt(float num)
+{
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = num * 0.5F;
+    y = num;
+    i = *(long *)&y;                   //floating point bit hack
+    i = 0x5f3759df - (i >> 1);         //magic
+    y = *(float *)&i;                  //back to float
+    y = y * (threehalfs - x2 * y * y); //1st iter
+    return y;
+}
